4abe2e677393742e69380e1b3b1fdd69733fdffa
akanola <folarin@getolyves.com>
2025-01-26 21:01:19 +0000

    three sums

--- three_sums.py
+++ three_sums.py
@@ -1,19 +1,32 @@
 def three_sums(nums):
-    result = []
-    storage = {}
-    for index_i, i in enumerate(nums):
-        remainder = 0 - i
-        for index_j, j in enumerate(nums):
-            if index_i == index_j:
-                pass
+    res = []
+    nums.sort()
+
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        j = i + 1
+        k = len(nums) - 1
+
+        while j < k:
+            num_i = nums[i]
+            num_j = nums[j]
+            num_k = nums[k]
+            total = num_i + num_j + num_k
+
+            if total > 0:
+                k -= 1
+            elif total < 0:
+                j += 1
             else:
-                what_left = remainder - j
-                if what_left == 0:
-                    print('i=', i, 'j=', j, 'remainder=', remainder)
-                else:
-                    storage[(remainder, j)] = (index_i, index_j)
-    return result
+                res.append([num_i, num_j, num_k])
+                j += 1
+
+                while nums[j] == nums[j - 1] and j < k:
+                    j += 1
+    return res
 
 
 if __name__ == '__main__':
-    print(three_sums(nums=[-1, 0, 1, 2, -1, -4]))
+    print(three_sums(nums=[1, 2, -2, -1, -1, 0, 0, 1, 2, -2, -1, 3, -3, 4, -4]))
